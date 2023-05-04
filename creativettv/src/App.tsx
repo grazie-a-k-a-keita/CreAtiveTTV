5a6baef9043c8b556c715f2fd4f63c32edcca142
Ono　Keita <oh.kitbsk.1028@icloud.com>
2023-05-04 07:06:31 +0000

    feat: スピーチ機能

--- creativettv/src/App.tsx
+++ creativettv/src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [speechText, setSpeechText] = useState<string>("");
+  const [speechPitch, setSpeechPitch] = useState<number>(1);
+  const [speechRate, setSpeechRate] = useState<number>(1);
+  const [speechVolume, setSpeechVolume] = useState<number>(1);
+
+  const utterThis = new SpeechSynthesisUtterance(speechText);
+
+  utterThis.lang = "ja-JP"; // lang
+  utterThis.pitch = speechPitch; // 0 ~ 2 Default 1
+  utterThis.rate = speechRate; // 0,1 ~ 10 Default 1
+  utterThis.volume = speechVolume; // 0 ~ 1 Default 1
+
+  utterThis.onerror = (event) => {
+    console.log(
+      `An error has occurred with the speech synthesis: ${event.error}`
+    );
+  };
+
+  const onclick = () => {
+    window.speechSynthesis.speak(utterThis);
+    setSpeechText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="range"
+        min="0"
+        max="2"
+        step="0.5"
+        onChange={(e) => setSpeechPitch(Number(e.target.value))}
+        value={speechPitch.toString()}
+      />
+      <input
+        type="range"
+        min="0"
+        max="2"
+        step="0.5"
+        onChange={(e) => setSpeechRate(Number(e.target.value))}
+        value={speechRate.toString()}
+      />
+      <input
+        type="range"
+        min="0"
+        max="1"
+        step="0.1"
+        onChange={(e) => setSpeechVolume(Number(e.target.value))}
+        value={speechVolume.toString()}
+      />
+      <input
+        type="text"
+        onChange={(e) => setSpeechText(e.target.value)}
+        value={speechText}
+      />
+      <button onClick={onclick}>speak</button>
+    </>
   );
 }
 
